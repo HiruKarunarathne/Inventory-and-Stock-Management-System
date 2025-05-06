6353e77224185ad5534bdda42a5b526355062583
Mevinu Methdam <mevinumethdam@yahoo.com>
2025-05-06 16:19:49 +0530

    Remove update and delete methods from ItemService.java

--- src/main/java/com/intellij/InventoryAndStockManagementSystem/service/ItemService.java
+++ src/main/java/com/intellij/InventoryAndStockManagementSystem/service/ItemService.java
@@ -7,30 +7,22 @@ import java.util.*;
 public class ItemService {
     private List<Item> items;
 
+
+
     public ItemService() {
         items = ItemFileUtil.readItems();
     }
+
+    // Create
     public void addItem(Item item) {
         items.add(item);
         ItemFileUtil.writeItems(items);
     }
 
+    // Read
     public List<Item> viewItems() {
         return items;
+
     }
 
-    public void updateItem(String id, Item updatedItem) {
-        for (int i = 0; i < items.size(); i++) {
-            if (items.get(i).getId().equals(id)) {
-                items.set(i, updatedItem);
-                break;
-            }
-        }
-        ItemFileUtil.writeItems(items);
-    }
-
-    public void deleteItem(String id) {
-        items.removeIf(item -> item.getId().equals(id));
-        ItemFileUtil.writeItems(items);
-    }
 }
