cbfc7afa1d437918c856bdeb082473ae82731093
nipunanaveen <nipunasnaveenn@gmail.com>
2025-05-21 22:19:52 +0530

    update changes

--- src/test/java/com/intellij/InventoryAndStockManagementSystem/InventoryAndStockManagementSystemApplicationTests.java
+++ src/test/java/com/intellij/InventoryAndStockManagementSystem/InventoryAndStockManagementSystemApplicationTests.java
@@ -1,13 +1,13 @@
-//package com.intellij.InventoryAndStockManagementSystem;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class InventoryAndStockManagementSystemApplicationTests {
-//
-//	@Test
-//	void contextLoads() {
-//	}
-//
-//}
+package com.intellij.InventoryAndStockManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryAndStockManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
