6afd4a3f21442d636445e38480f351e6618193b4
ChamidiLak <dchamidiwijesuriya@gmail.com>
2025-05-23 00:21:24 +0530

    User controller changes

--- src/main/java/com/intellij/InventoryAndStockManagementSystem/controller/ManageUserController.java
+++ src/main/java/com/intellij/InventoryAndStockManagementSystem/controller/ManageUserController.java
@@ -1,28 +1,23 @@
-package com.intellij.InventoryAndStockManagementSystem.controller;
-public class ManageUserController {
-}
 
-//import com.intellij.InventoryAndStockManagementSystem.model.ManageUser;
-//import com.intellij.InventoryAndStockManagementSystem.service.ManageUserService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.*;
-//
-//import java.util.List;
-//
-//@CrossOrigin(origins = "*")
-//@RestController
-//public class ManageUserController {
-//
-//    @Autowired
-//    private ManageUserService userService;
-//
-//    @PostMapping("/signup")
-//    public void signup(@RequestBody ManageUser user) {
-//        userService.saveUser(user);
-//    }
-//
-//    @GetMapping("/users")
-//    public List<ManageUser> getAllUsers() {
-//        return userService.getAllUsers();
-//    }
-//}
+package com.intellij.InventoryAndStockManagementSystem.controller;
+
+import com.intellij.InventoryAndStockManagementSystem.model.ManageUser;
+import com.intellij.InventoryAndStockManagementSystem.service.ManageUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+@CrossOrigin(origins = "*")
+public class ManageUserController {
+
+    @Autowired
+    private ManageUserService manageUserService;
+
+    @GetMapping
+    public List<ManageUser> getAllUsers() {
+        return manageUserService.getAllUsers();
+    }
+}
--- src/main/java/com/intellij/InventoryAndStockManagementSystem/service/impl/ManageUserServiceImpl.java
+++ src/main/java/com/intellij/InventoryAndStockManagementSystem/service/impl/ManageUserServiceImpl.java
@@ -1,27 +1,43 @@
 package com.intellij.InventoryAndStockManagementSystem.service.impl;
 
-public class ManageUserServiceImpl {
-}
-//import com.intellij.InventoryAndStockManagementSystem.model.ManageUser;
-//import com.intellij.InventoryAndStockManagementSystem.service.ManageUserService;
-//import org.springframework.stereotype.Service;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//@Service
-//public class ManageUserServiceImpl implements ManageUserService {
-//
-//    private List<ManageUser> users = new ArrayList<>();
-//
-//    @Override
-//    public List<ManageUser> getAllUsers() {
-//        return users;
-//    }
-//
-//    @Override
-//    public void saveUser(ManageUser user) {
-//        users.add(user);
-//    }
-//}
+import com.intellij.InventoryAndStockManagementSystem.model.ManageUser;
+import com.intellij.InventoryAndStockManagementSystem.service.ManageUserService;
+import org.springframework.stereotype.Service;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ManageUserServiceImpl implements ManageUserService {
+
+    private static final String FILE_PATH = "users.txt";
+
+    @Override
+    public void saveUser(ManageUser user) {
+        try (FileWriter fw = new FileWriter(FILE_PATH, true)) {
+            fw.write(user.getName() + "," + user.getPhone() + "," + user.getEmail() + "," +
+                    user.getUsername() + "," + user.getPassword() + "," + user.getImage() + "\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public List<ManageUser> getAllUsers() {
+        List<ManageUser> users = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(FILE_PATH))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] parts = line.split(",", 6);
+                if (parts.length == 6) {
+                    users.add(new ManageUser(parts[0], parts[1], parts[2], parts[3], parts[4], parts[5]));
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return users;
+    }
+
+}
